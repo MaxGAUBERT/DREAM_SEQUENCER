c4a8a58c2c1cfddcd1438bc73ffa1f97279d9140
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-05-29 12:07:45 +0200

    Add files via upload

--- src/Components/Contexts/ItemsToMapForDisplay.js
+++ src/Components/Contexts/ItemsToMapForDisplay.js
@@ -2,10 +2,8 @@ export const itemsToMapForDisplay = () => {
 
     const items = ["New", "SaveAs", "Save", "Load", "Settings", "Quit", "Undo", "Redo", 
         "ChannelRack", "Browser", "Playlist", "AnalogSynth", "Modulator", 
-        "ChRackUpload", "ChRackPiano", "ChRackDelete", "ChRackRename", "ChRackCreate", "ChRackAdd",
+        "ChRackUpload", "ChRackPiano", "ChRackDelete", "ChRackRename", "ChRackCreate", "ChRackAdd", "ChReset",
         "Play Song", "Stop Song", "Play Pattern", "Stop Pattern", "Record", "Replay", "Clear", "BPM", "LoopMode", "SongMode", "PatternMode",
-
-
     ];
 
     return items;
