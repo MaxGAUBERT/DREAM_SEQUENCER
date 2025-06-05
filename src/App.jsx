c2ae5502e2f4c985378ac017e5deb762fd19555e
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-06-05 12:08:32 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NewProject from "./Components/SystemTools/NewProject";
+import NewProject from "./Components/StorageManager/HomePage";
 import ChannelRack from "./Components/ComplexComponents/ChannelRack";
 import StripMenu from "./Components/FrontEnd/StripMenu";
 import Home from "./Components/Home";
 import Grid from "./Components/ComplexComponents/PianoRoll";
 import PatternManager from "./Components/ComplexComponents/PatternManager";
 import "./App.css"; // ✅ Import ici pour appliquer à toute l'application
-import { Box} from "@mui/material";
 import LaunchAnimation from "./Components/FrontEnd/LaunchAnimation";
-import Playlist from "./Components/ComplexComponents/Playlist";
-import ProjectManager from "./Components/SystemTools/ProjectManager";
+import ProjectLoader from "./Components/StorageManager/ProjectLoader";
 import ColorProvider from "./Components/Contexts/ColorProvider";
 import MainPanel from "./Components/FrontEnd/MainPanel";
+import HomePage from "./Components/StorageManager/HomePage";
 function App() {
 
     return (
@@ -20,16 +19,15 @@ function App() {
             <BrowserRouter>
                 <div className="flex flex-col h-100vh">
                     <Routes>
-                        <Route path="/" element={<NewProject />} />
+                        <Route path="/" element={<HomePage />} />
                         <Route path="/LaunchAnimation" element={<LaunchAnimation />} />
                         <Route path="/Home" element={<Home />} />
                         <Route path="/StripMenu" element={<StripMenu />} />
                         <Route path="/ChannelRack" element={<ChannelRack />} />
                         <Route path="/Grid" element={<Grid />} />
                         <Route path="/PatternManager" element={<PatternManager />} />
-                        <Route path="/NewProject" element={<NewProject />} />
-                        <Route path="/Playlist" element={<Playlist />} />
-                        <Route path="/ProjectManager" element={<ProjectManager />} />
+                        <Route path="/HomePage" element={<HomePage />} />
+                        <Route path="/ProjectLoader" element={<ProjectLoader />} />
                         <Route path="/MainPanel" element={<MainPanel />} />
                     </Routes>
                 </div>
