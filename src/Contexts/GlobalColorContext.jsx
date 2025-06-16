0a0565c19d3aaaa8d45b832c422da53bf946ee8a
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-06-16 19:25:30 +0200

    Add files via upload

--- src/Contexts/GlobalColorContext.jsx
+++ src/Contexts/GlobalColorContext.jsx
@@ -8,7 +8,7 @@ export const useGlobalColorContext = () => useContext(GlobalColorContext);
 
 const GlobalColorContextProvider = ({ children }) => {
     const [colorsComponent, setColorsComponent] = useState({
-        "Text": "gray.700",
+        "Text": "white",
         "Button": "white",
         "Background": "gray.900",
         "Border": "#ffffff"
