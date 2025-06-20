89ff101dcb83933c90d48bb7f3571626031cf040
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-06-20 10:49:55 +0200

    Add files via upload

--- src/Contexts/PlayContext.jsx
+++ src/Contexts/PlayContext.jsx
@@ -1,6 +1,5 @@
 import { useContext, createContext, useRef, useState } from "react";
-
-
+import * as Tone from "tone";
 
 const CreatePlayContext = createContext(null);
 export const usePlayContext = () => useContext(CreatePlayContext);
@@ -12,9 +11,17 @@ const PlayContext = ({ children }) => {
     const [isPlaying, setIsPlaying] = useState(false);
     const [bpm, setBpm] = useState(140);
     const [volume, setVolume] = useState(0);
+    const [metronome, setMetronome] = useState(false);
+    const [metronomeSampler, setMetronomeSampler] = useState(
+      new Tone.Sampler({
+        urls: { C4: "metronome.mp3" }, 
+        baseUrl: "/Audio/", 
+        onload: () => console.log("Metronome loaded")
+      }).toDestination()
+    );
 
     return (
-        <CreatePlayContext.Provider value={{samplerRef, sequencesRef, isPlaying, setIsPlaying, bpm, setBpm, volume, setVolume}}>
+        <CreatePlayContext.Provider value={{samplerRef, sequencesRef, isPlaying, setIsPlaying, bpm, setBpm, volume, setVolume, metronome, setMetronome, metronomeSampler, setMetronomeSampler}}>
             {children}
         </CreatePlayContext.Provider>
     );
