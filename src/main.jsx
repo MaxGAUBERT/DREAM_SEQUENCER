f9d8c389d2f071b3e094c10f23552abbbf935631
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-12-07 12:20:58 +0100

    added theme system

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./index.css"
-import App from './App'
-import { HistoryProvider } from './Contexts/HistoryProvider'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HistoryProvider } from "./Contexts/HistoryProvider";
+import { SettingsProvider } from "./Contexts/SettingsContexts";
+import { ThemeProvider } from "next-themes";
+import { ThemeProviderContext } from "./Contexts/ThemeContext";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HistoryProvider>
-    <App />
-    </HistoryProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <ThemeProviderContext>
+        <SettingsProvider>
+          <HistoryProvider>
+            <App />
+          </HistoryProvider>
+        </SettingsProvider>
+      </ThemeProviderContext>
+    </ThemeProvider>
   </StrictMode>
 );
